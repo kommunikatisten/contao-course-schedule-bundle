6b7edf239c6f8701c006c7cdddd968c07335afaa
frfr <frfr@spreadshirt.net>
2020-11-07 20:00:19 +0100

    adds missing usage

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -6,6 +6,7 @@ namespace Kommunikatisten\ContaoScheduleBundle\Controller;
 
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\Exception\RedirectResponseException;
+use Contao\CoreBundle\ServiceAnnotation\FrontendModule;
 use Contao\ModuleModel;
 use Contao\PageModel;
 use Contao\Template;
