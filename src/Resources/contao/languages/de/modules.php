0adb80e674129511af79ecb5cd827e232c6648a8
frfr <frfr@spreadshirt.net>
2020-11-09 17:55:09 +0100

    adds new controller

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -1,4 +1,5 @@
 <?php
 
-$GLOBALS['TL_LANG']['FMD']['MOD.schedule'] = ['KursWochenPlan', 'Zeigt den Kursplan für eine Woche an.'];
-$GLOBALS['TL_LANG']['FMD']['kom_test'] = ['TestModule', 'Zeigt nichts weiter an.'];
+$GLOBALS['TL_LANG']['FMD']['komm_schedule'] = ['Kurs Wochen Plan', 'Zeigt den Kursplan für eine Woche an.'];
+$GLOBALS['TL_LANG']['FMD']['komm_course_list'] = ['Kurs Wochen List', 'Zeigt nichts weiter an.'];
+$GLOBALS['TL_LANG']['FMD']['komm_course_schedule_short'] = ['Kurs Wochen Plan Kurz', 'Zeigt nichts weiter an.'];
