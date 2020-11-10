617e51c4acc83af1ddf75a0f16ab3ee2bc6d2bde
frfr <frfr@spreadshirt.net>
2020-11-10 12:35:56 +0100

    check path to twig

--- src/Controller/BE/SubjectController.php
+++ src/Controller/BE/SubjectController.php
@@ -5,6 +5,7 @@ namespace Kommunikatisten\ContaoScheduleBundle\Controller\BE;
 
 use Contao\CoreBundle\Controller\AbstractController;
 use Contao\CoreBundle\Framework\ContaoFramework;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Contao\CoreBundle\Framework\FrameworkAwareInterface;
@@ -21,9 +22,11 @@ class SubjectController extends AbstractController implements FrameworkAwareInte
 
     private ?ContaoFramework $framework;
     private TwigEnvironment $twig;
+    private LoggerInterface $logger;
 
-    public function __construct(TwigEnvironment $twig) {
+    public function __construct(TwigEnvironment $twig, LoggerInterface $logger) {
         $this->twig = $twig;
+        $this->logger = $logger;
     }
 
     /**
@@ -31,10 +34,16 @@ class SubjectController extends AbstractController implements FrameworkAwareInte
      * @throws Error
      */
     public function __invoke(): Response {
+        foreach ($this->twig->getGlobals() as $k => $gl) {
+            $this->logger->info($k + ': ' + $gl);
+        }
+        return new Response('<p>it s me </p>');
+        /*
         return new Response($this->twig->render(
             '../vendor/kommunikatisten/Resources/views/backend_subjects.html.twig',
             []
         ));
+        */
     }
 
     public function setFramework(ContaoFramework $framework = null) {
