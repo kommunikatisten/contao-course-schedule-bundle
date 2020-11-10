c85d54ebba90980f126977bdf0eb0011c884adb8
frfr <frfr@spreadshirt.net>
2020-11-10 12:53:55 +0100

    rm BE controller

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,8 +1,10 @@
 <?php
 
 $GLOBALS['BE_MOD']['content']['komm_course'] = [
-    'tables' => ['tl_komm_course'],
+    'tables' => ['tl_komm_course', 'tl_content'],
+    'table' => array('Contao\TableWizard', 'importTable'),
 ];
 $GLOBALS['BE_MOD']['content']['komm_subject'] = [
-    'tables' => ['tl_komm_subject'],
+    'tables' => ['tl_komm_subject', 'tl_content'],
+    'table' => array('Contao\TableWizard', 'importTable'),
 ];
